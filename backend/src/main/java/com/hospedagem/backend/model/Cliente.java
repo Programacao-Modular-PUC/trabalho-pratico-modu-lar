61b6aa4e6f00174a90d7dfa02c0971a5e40c8950
Nevez6 <arturfneves17@gmail.com>
2026-05-17 11:20:42 -0300

    persistencia em MySQL feita

---
+++ backend/src/main/java/com/hospedagem/backend/model/Cliente.java
@@ -0,0 +1,25 @@
+package com.hospedagem.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
